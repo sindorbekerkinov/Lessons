2cab7e240168e2f24761c22e07c3a5fd4c68b796
Sindorbek <sindorbekerkinov@gmail.com>
2025-02-15 16:10:57 +0500

    add

---
+++ Lesson_11_part2.py
@@ -0,0 +1,91 @@
+# import Lesson_11 as l_11
+#
+# avto1 = l_11.avto_info("GM","Malibu","Qora","avtomat",2019,222222)
+# l_11.info_print(avto1)
+
+
+# from Lesson_11 import avto_info, info_print
+#
+# avto1 = avto_info("GM","Malibu","Qora","avtomat",2019,222222)
+# info_print(avto1)
+#
+
+
+# from Lesson_11 import avto_info as ai, info_print as ip, avto_kirit as ak
+#
+# avto1 = ai("GM","Malibu","Qora","avtomat",2019,222222)
+# ip(avto1)
+
+
+# from Lesson_11 import *
+# avto1 = avto_info("GM","Malibu","Qora","avtomat",2019,222222)
+# info_print(avto1)
+#
+
+
+# from math import sqrt
+# x=400
+# print(sqrt(x))
+
+
+# print(pow(2,3))
+
+#
+# from math import pi,log2,log10,log
+# print(pi)
+# print(pow(5,5))
+# print(log2(8))
+# print(log10(100))
+# print(log(100,2))
+
+
+# import random as r
+# son = r.randint(0,1000)
+# print(son)
+# ismlar = ['olim', 'anvar', 'hasan','husan']
+# ism = r.choice(ismlar)
+# print(ism)
+# print(r.choices(ism))
+
+# import random as r
+# x = list(range(0,51,5))
+# print(x)
+# print(r.choice(x))
+#
+# x= list(range(11))
+# print(x)
+# r.shuffle(x)
+# print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
